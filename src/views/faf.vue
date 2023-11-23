f82f77db5af37126c5831ccdd67145c74dbf53cf
Selman Kaya <selmankaya@Selman-Air.lan>
2023-11-23 20:35:10 +0300

    ilk yorum

---
+++ src/views/faf.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class=" bg-slate-400">
+        <p class=" font-semibold ">FAST AND FURİOUS</p>
+        <div class=" bg-slate-300 text-white" >
+            <img src="https://tr.web.img3.acsta.net/r_654_368/img/61/78/61789d8ab204343d647d8275d38ba9e4.jpg" alt="hızlı ve ofkelı">
+        </div>
+        <div class=" bg-slate-200 ">
+        
+        </div>
+        <div>
+            <div class="bg-gray-200 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-md max-w-md">
+      <h1 class="text-3xl font-bold mb-4">Fast and Furious Hakkında</h1>
+      <p class="text-lg mb-4">Hızlı ve Öfkeli bir Amerikalı yasadışı sokak yarışı ve silahlı soygun merkezli, aksiyon filmleri ve diğer çeşitli medya filmlerinden oluşan bir seridir. Universal Pictures tarafından dağıtılan ve ilk filmi 2001'de çıkan seri dünya çapında büyük hasılat elde etmiş ve izleyicilerin beğenisini toplamıştır.</p>
+      <p class="text-sm text-gray-600 mb-2">Yönetmenler:  Vin Diesel, Justin Lin, Louis Leterrier, James Wan, F. Gary Gray, Rob Cohen, John Singleton, Philip Atwell</p>
+      <p class="text-sm text-gray-600 mb-4">Oyuncular: Jane Smith, Michael Johnson</p>
+      <p class="text-sm text-gray-600 mb-4">Hasılat: 5 Milyar 891 Milyon $</p>
+      <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Daha Fazla Bilgi</a>
+    </div>
+  </div>
+        </div>
+    </div>
+    
+</template>
